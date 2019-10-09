2daeef9525f6af11617f443b74b83e5731ab6a4d
AdriGV <adrian.gimeno.vigo@gmail.com>
2019-10-09 12:26:41 +0200

    :tada: Git inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,27 @@
+/* 
+ * Copyright 2019 AdrianGV  adrian.gimeno.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author AdrianGV adrian.gimeno.alum@iescamp.es
+ */
+public final class Main {
+    public static void main (String[] args){
+        boolean HORARIO_INVIERNO = false;
+        System.out.printf("Horario de invierno...: %b.%n" , HORARIO_INVIERNO);
+    }
+}
